6b04ca0767aec39b55308cc63648d444ccac6f9e
FrankBarretta <francescobarretta@outlook.it>
2025-04-15 13:13:01 +0200

    Fixed Backend Test

--- mongoDbRepository/backend/server.js
+++ mongoDbRepository/backend/server.js
@@ -4,11 +4,13 @@ const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
-const postsRouter = require('./routes/posts');
 
-app.use('/posts', postsRouter);
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // per leggere i body JSON
+
+// 👉 Poi le rotte
+const postsRouter = require('./routes/posts');
+app.use('/posts', postsRouter);
 
 mongoose.connect('mongodb://localhost:27017', {
   useNewUrlParser: true,
