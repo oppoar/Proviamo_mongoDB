805a0a0b6237e95e6bfbfc16f2da1f6b2111f250
Oppo <armando.oppo@capgemini.com>
2025-09-17 10:58:10 +0200

    modifiche

--- mongoDbRepository/src/app/components/armando-component/armando-component.component.ts
+++ mongoDbRepository/src/app/components/armando-component/armando-component.component.ts
@@ -6,60 +6,55 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-armando-component',
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './armando-component.component.html',
-  styleUrl: './armando-component.component.scss'
+  styleUrl: './armando-component.component.scss',
 })
 export class ArmandoComponentComponent implements OnInit {
   immagini: Immagine[] = [];
 
   nuovaImmagine: Partial<Immagine> = {
     img: '',
-    size: 0
+    size: 0,
   };
 
   tagInput: string = '';
   private idCounter = 1;
 
-  constructor(private apiImmagine: ImmagineService){}
+  constructor(private apiImmagine: ImmagineService) {}
 
-  
   ngOnInit(): void {
     this.getAllImmagini();
   }
-    
+
   getAllImmagini(): void {
     this.apiImmagine.getAllImmagini().subscribe({
       next: (response) => {
-      this.immagini = response;
-      console.log('Immagini recuperate con successo:', response);
-    },
+        this.immagini = response;
+        console.log('Immagini recuperate con successo:', response);
+      },
       error: (err) => {
         console.error('Errore durante il recupero delle immagini:', err);
-      }
+      },
     });
-  }  
+  }
 
   onSubmit() {
     const nuovaImg: Immagine = {
       idImg: this.idCounter++,
       img: this.nuovaImmagine.img || '',
       size: this.nuovaImmagine.size || 0,
-      tag: this.tagInput ? this.tagInput.split(',').map(t => t.trim()) : []
+      tag: this.tagInput ? this.tagInput.split(',').map((t) => t.trim()) : [],
     };
 
     this.apiImmagine.addImmagine(nuovaImg).subscribe({
       next: (response) => {
-        console.log('Immagine salvata con successo:', response);
-        this.immagini.push(response); 
+        this.immagini.push(response);
       },
       error: (err) => {
         console.error('Errore durante il salvataggio:', err);
-      }
+      },
     });
-
-    console.log(nuovaImg);
-
     this.nuovaImmagine = { img: '', size: 0 };
     this.tagInput = '';
   }
